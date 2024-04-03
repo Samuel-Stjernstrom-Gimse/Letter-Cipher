5295d958efaf38c5c7f3fc60365d5c89be0603bb
Zam <samuel.kodehode@gmail.com>
2024-04-03 11:47:04 +0200

    init

--- script.ts
+++ script.ts
@@ -3,45 +3,45 @@ const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 const dataArray: number[][] = []
 let rowArray: number[] = []
-let columnsCounter = 0
-let isDrawing = false; // Flag to track drawing state
+let columnsCounter: number = 0
+let isDrawing: boolean = false
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (): void => {
     makeArray()
 })
 if (ctx) {
-    canvas.addEventListener('mousedown', (e) => {
+    canvas.addEventListener('mousedown', (e: MouseEvent): void => {
         isDrawing = true;
         ctx.beginPath();
         ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     });
 
-    canvas.addEventListener('mousemove', (e) => {
+    canvas.addEventListener('mousemove', (e: MouseEvent): void => {
         if (isDrawing) {
             ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
             ctx.stroke();
         }
     });
 
-    canvas.addEventListener('mouseup', () => {
-        isDrawing = false;
+    canvas.addEventListener('mouseup', (): void => {
+        isDrawing = false
     })
 } else {
     console.log(69)
 }
 
-const makeArray = () => {
+const makeArray = (): void => {
     if (ctx) {
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const pixelData = imageData.data;
-        for (let i = 0; i < pixelData.length; i += 4) {
+        const imageData: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const pixelData: Uint8ClampedArray = imageData.data;
+        for (let i: number = 0; i < pixelData.length; i += 4) {
             columnsCounter += 1
             if (columnsCounter === 100) {
                 columnsCounter = 0
                 dataArray.push(rowArray)
-                rowArray = []; // Clear rowArray after pushing its contents
+                rowArray = []
             }
-            const pixelSum = pixelData[i] + pixelData[i + 1] + pixelData[i + 2] + pixelData[i + 3];
+            const pixelSum: number = pixelData[i] + pixelData[i + 1] + pixelData[i + 2] + pixelData[i + 3];
             rowArray.push(pixelSum === 0 ? 0 : 1);
         }
         console.log(dataArray);
